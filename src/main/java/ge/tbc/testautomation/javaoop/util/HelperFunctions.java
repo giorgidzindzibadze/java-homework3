db029af428f9bbcaeed6a43714ff82eb6889ab68
giorgidzindzibadze <giorgi.dzindzibadze.1@btu.edu.ge>
2023-07-31 19:50:38 +0400

    Last commit

--- src/main/java/ge/tbc/testautomation/javaoop/util/HelperFunctions.java
+++ src/main/java/ge/tbc/testautomation/javaoop/util/HelperFunctions.java
@@ -1,21 +1,21 @@
-package ge.tbc.testautomation.javaoop.util;
-
-import ge.tbc.testautomation.javaoop.figures.Rectangle;
-
-public class HelperFunctions {
-    public static void compareRectangles(Rectangle rect1, Rectangle rect2){
-        double perimeter1= rect1.getPerimeter();
-        double perimeter2=rect2.getPerimeter();
-
-        if (perimeter1>perimeter2){
-            System.out.println("parameter 1 > parameter 2");
-        } else if (perimeter1<perimeter2) {
-            System.out.println("parameter 1 < parameter 2");
-
-        }else {
-            System.out.println("parameter 1 = parameter 2");
-        }
-
-    }
-
-}
+//package ge.tbc.testautomation.javaoop.util;
+//
+//import ge.tbc.testautomation.javaoop.figures.Rectangle;
+//
+//public class HelperFunctions {
+//    public static void compareRectangles(Rectangle rect1, Rectangle rect2){
+//        double perimeter1= rect1.getPerimeter();
+//        double perimeter2=rect2.getPerimeter();
+//
+//        if (perimeter1>perimeter2){
+//            System.out.println("parameter 1 > parameter 2");
+//        } else if (perimeter1<perimeter2) {
+//            System.out.println("parameter 1 < parameter 2");
+//
+//        }else {
+//            System.out.println("parameter 1 = parameter 2");
+//        }
+//
+//    }
+//
+//}
